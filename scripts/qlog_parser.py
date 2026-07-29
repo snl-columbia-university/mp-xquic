41956c5f449eb0ef38d2ee9674c8be6ed18f9792
Carson Garland <73087750+carsongarland@users.noreply.github.com>
2026-07-29 16:18:44 -0400

    adding support for qlogging (#9)

--- scripts/qlog_parser.py
+++ scripts/qlog_parser.py
@@ -33,11 +33,6 @@ packet_type_ = {0: "initial",  1: "0RTT", 2: "handshake" ,
 
 packet_number_namespace_ = {0: "initial", 1: "handshake", 2: "application data", 3: "negotiation"}
 
-packet_type_ = {0: "initial",  1: "0RTT", 2: "handshake" ,
-                3: "retry", 4: "short_header", 5: "version_negotiation", 6: "unknown"}
-
-packet_number_namespace_ = {0: "initial", 1: "handshake", 2: "application data", 3: "negotiation"}
-
 send_stream_states_ = ["ready", "send", "data_sent", "reset_sent", "reset_received"]
 
 recv_stream_states_ = ["receive", "size_known", "data_read", "reset_read", "reset_received", "reset_received"]
@@ -112,14 +107,13 @@ def parse_line(line):
     if matches[1] in  ["server_listening", "connection_started", "connection_close", "connection_state_updated",
                         "path_assigned", "mtu_updated", "alpn_information","parameters_set", "packet_buffered", "packets_acked", "stream_state_updated",
                         "frames_processed", "stream_data_moved", "rec_parameters_set", "rec_metrics_updated", "congestion_state_updated", 
-                        "packet_lost", "http_parameters_set", "http_frame_created", "http_frame_parse"]:
+                        "packet_lost", "http_parameters_set", "http_frame_created", "http_frame_parsed"]:
         function_name = "parse_" + matches[1]
         func = globals()[function_name]
         (event["data"], event_scid) = func(line)
         return event, event_scid
     else:
         return None, None
-
 
 
 def endpoint_events_extraction(file_name, vantagepoint):
@@ -222,7 +216,6 @@ def parse_server_listening(line):
     return (data, event_scid)
 
 def parse_connection_started(line):
-     # [2024/05/14 11:34:11 641605] [connection_started] |scid:b59e52a51185db48|xqc_engine_packet_process|local|src_ip:127.0.0.1|src_port:35148|
     data = {
             "src_ip": "127.0.0.1",
             "dst_ip": "127.0.0.1",
@@ -251,7 +244,6 @@ def parse_connection_started(line):
     return (data, event_scid)
 
 def parse_connection_closed(line):
-    # [2024/05/14 11:34:26 672332] [connection_closed] |scid:007cc254f81be8e78d765a2e63339fc99a66320d|xqc_conn_destroy|err_code:0|
     data = {
             "connection_code": 0
     }
@@ -337,8 +329,6 @@ def parse_mtu_updated(line):
     return (data, event_scid)
 
 def parse_alpn_information(line):
-    #[2024/05/14 11:34:11 548027] [alpn_information] 
-    #|scid:007cc254f81be8e78d765a2e63339fc99a66320d|xqc_ssl_alpn_select_cb|client_alpn:h3 |server_alpn:h3 h3-29 h3-ext transport |selected_alpn:h3|
     data = {
             "server_alpns": [],
             "client_alpns": [],
@@ -371,8 +361,6 @@ def parse_alpn_information(line):
 
 
 def parse_parameters_set(line):
-    # [2024/05/14 11:34:11 547473] [tra_parameters_set] |scid:007cc254f81be8e78d765a2e63339fc99a66320d|
-    # xqc_conn_create|local|migration:1|max_idle_timeout:120000|max_udp_payload_size:1500|active_connection_id_limit:8|max_data:0|
     data = {
             "max_idle_timeout": 0,
             "max_udp_payload_size": 0,
@@ -389,13 +377,11 @@ def parse_parameters_set(line):
             continue
         if item[0] == "scid":
             event_scid = item[1]
-        elif item[0] in data.keys:
+        elif item[0] in data:
             data[item[0]] = int(item[1])
     return (data, event_scid)
 
 def parse_packet_buffered(line):
-    # [2024/05/14 11:34:11 693524] [packet_buffered] |scid:007cc254f81be8e78d765a2e63339fc99a66320d|
-    # xqc_conn_buff_undecrypt_packet_in|pkt_pns:2|pkt_type:4|len:1216|
     data = {
             "header": {
               "packet_number": "unknown",
@@ -425,8 +411,6 @@ def parse_packet_buffered(line):
     return (data, event_scid)
 
 def parse_packets_acked(line):
-    # [2024/05/14 11:34:11 642140] [packets_acked] |scid:007cc254f81be8e78d765a2e63339fc99a66320d|
-    # xqc_process_ack_frame|pkt_space:0|high:0|low:0|path_id:0|
     data = {
             "packet_number_space": "unknown",
             "packet_numbers": []
@@ -457,7 +441,6 @@ def parse_packets_acked(line):
     return (data, event_scid)
 
 def parse_datagrams_sent_or_recv(line):
-    # [2024/05/14 11:34:11 552331] [datagrams_sent] |scid:007cc254f81be8e78d765a2e63339fc99a66320d|xqc_send|size:1216|
     data = {
             "raw": {
               "length": 1280
@@ -479,8 +462,6 @@ def parse_datagrams_sent_or_recv(line):
     return (data, event_scid)
 
 def parse_stream_state_updated(line):
-    # [2024/05/14 11:34:11 552613] [stream_state_updated] 
-    # |scid:007cc254f81be8e78d765a2e63339fc99a66320d|xqc_stream_send_state_update|stream_id:3|send_stream|old:0|new:1|
     data = {
            "StreamType" : "bidirectional",
            "new": "unknown",
@@ -513,7 +494,7 @@ def parse_stream_state_updated(line):
 
 def parse_frames_processed(line):
     data = {
-           "frames":[{"frame_type" : "unknow"}]
+           "frames":[{"frame_type" : "unknown"}]
     }
     event_scid = "unknown"
     segments = line.split('|')
@@ -528,8 +509,13 @@ def parse_frames_processed(line):
         if(len(item) == 2 and item[0] == "type"):
             frame_type = int(item[1])
             break
-    data["frames"][0]["frame_type"] = frame_type_[frame_type].lower()
-    type_str = frame_type_[frame_type]
+
+    if 0 <= frame_type < len(frame_type_):
+        type_str = frame_type_[frame_type]
+        data["frames"][0]["frame_type"] = type_str.lower()
+    else:
+        type_str = f"UNKNOWN_{frame_type}"
+        data["frames"][0]["frame_type"] = type_str.lower()
 
     if type_str == "PADDING":
         for i in range(1,len(segments)):
@@ -552,9 +538,9 @@ def parse_frames_processed(line):
             if item[0] == "ack_range":
                 s_clean = item[1].strip("{}")  
                 pairs = s_clean.split(", ")  
-                result = [list(map(int, pair.split(" - "))) for pair in pairs]
+                result = [list(map(int, pair.split(" - "))) for pair in pairs if pair]
                 for i in range(len(result)):
-                    if result[i][0] == result[i][1]:
+                    if len(result[i]) == 2 and result[i][0] == result[i][1]:
                         result[i] = [result[i][0]]
                 data["frames"][0]["acked_ranges"] = result
         return (data, event_scid)
@@ -608,7 +594,7 @@ def parse_frames_processed(line):
             elif item[0] == "fin":
                 data["frames"][0]["fin"] = False
                 if int(item[1]):
-                    data["frames"][0]["offset"] = True
+                    data["frames"][0]["fin"] = True
         return (data, event_scid)
     
     elif type_str == "MAX_DATA":
@@ -704,8 +690,6 @@ def parse_frames_processed(line):
         return (data, event_scid)
 
 def parse_stream_data_moved(line):
-    # [2024/05/14 11:34:11 547904] [stream_data_moved] |scid:007cc254f81be8e78d765a2e63339fc99a66320d|xqc_stream_send|ret:-610|stream_id:3|stream_send_offset:0|pkt_type:SHORT_HEADER|buff_1rtt:0|send_data_size:1|offset:0|fin:0|
-    # stream_flag:17|conn:0000000002354F7C|conn_state:S_INIT|flag:TICKING TOKEN_OK UPPER_CONN_EXIST INIT_RECVD |from:application|to:transport|
     data = {
            "stream_id" : "unknown",
            "offset": 0,
@@ -755,9 +739,6 @@ def parse_rec_parameters_set(line):
     return (data, event_scid)
 
 def parse_rec_metrics_updated(line):
-    # [2024/05/14 11:34:11 642348] [rec_metrics_updated] 
-    # |scid:007cc254f81be8e78d765a2e63339fc99a66320d|xqc_send_ctl_on_ack_received|
-    # cwnd:47152|inflight:1384|mode:0|applimit:0|pacing_rate:1477151|bw:13370|srtt:89748|latest_rtt:89748|ctl_rttvar:0|pto_count:89748|min_rtt:6|send:0|lost:0|tlp:29|recv:47152|
     data = {}
     event_scid = "unknown"
     segments = line.split('|')
